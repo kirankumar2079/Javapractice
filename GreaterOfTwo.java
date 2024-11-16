2a05bdc29b0642fb20ccdbf4c79f287e52af07f6
kirankumar <kiran09082001@gmail.com>
2024-11-16 18:46:35 +0530

    greater of two numbers

---
+++ GreaterOfTwo.java
@@ -0,0 +1,13 @@
+public class GreaterOfTwo {
+    public static void main(String[] args)
+    {
+        int a = 10 , b = 20 ;
+        if (a > b)
+        {
+            System.out.println("a is greater");
+        }
+        else{
+            System.out.print("b is greater");
+        }
+    }
+}
