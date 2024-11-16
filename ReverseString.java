28454819d126b092508b742745b43ae72421f72d
kirankumar <kiran09082001@gmail.com>
2024-11-16 22:27:11 +0530

    basic programs

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ReverseString {
+
+    public String reverseString(String s , int i , int j)
+    {
+
+    }
+    public static void main(String[] args)
+    {
+        String text ;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        text = sc.next();
+
+        System.out.println("Reverse of the string " + text);
+
+    }
+}
